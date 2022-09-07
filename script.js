7117283dbea558a74cbebbd5899cc443f02f1fd8
Naresh Rajanbabu <98876740+Naresh2995@users.noreply.github.com>
2022-09-07 11:49:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const boxesContainer = document.getElementById('boxes')
 const btn = document.getElementById('btn')
 
@@ -15,22 +14,4 @@ function createBoxes() {
   }
 }
 
-=======
-const boxesContainer = document.getElementById('boxes')
-const btn = document.getElementById('btn')
-
-btn.addEventListener('click', () => boxesContainer.classList.toggle('big'))
-
-function createBoxes() {
-  for (let i = 0; i < 4; i++) {
-    for (let j = 0; j < 4; j++) {
-      const box = document.createElement('div')
-      box.classList.add('box')
-      box.style.backgroundPosition = `${-j * 125}px ${-i * 125}px`
-      boxesContainer.appendChild(box)
-    }
-  }
-}
-
->>>>>>> 923191284a455a350c4e31dafaa2583aa9851ba8
 createBoxes()
